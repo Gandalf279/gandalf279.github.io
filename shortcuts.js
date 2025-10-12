f615d6040e63aae370bfb784b4c057d0db3ce7b5
Carl Johann Stempel <carljohann@robotik.ag>
2025-10-12 03:45:25 +0200

    added shortcuts page

---
+++ shortcuts.js
@@ -0,0 +1,29 @@
+const container = document.getElementById("command-field");
+const cursor = document.querySelector(".cursor");
+
+const command = "shortcuts";
+const result = `
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" className="size-6" style="height: 1em; width: 1em"><path strokeLinecap="round" strokeLinejoin="round" d="m15.75 10.5 4.72-4.72a.75.75 0 0 1 1.28.53v11.38a.75.75 0 0 1-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 0 0 2.25-2.25v-9a2.25 2.25 0 0 0-2.25-2.25h-9A2.25 2.25 0 0 0 2.25 7.5v9a2.25 2.25 0 0 0 2.25 2.25Z" /></svg> <a href="https://youtube.com" target="_blank">YouTube</a>
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="size-6" style="height: 1em; width: 1em"><path stroke-linecap="round" stroke-linejoin="round" d="M2.25 13.5h3.86a2.25 2.25 0 0 1 2.012 1.244l.256.512a2.25 2.25 0 0 0 2.013 1.244h3.218a2.25 2.25 0 0 0 2.013-1.244l.256-.512a2.25 2.25 0 0 1 2.013-1.244h3.859m-19.5.338V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18v-4.162c0-.224-.034-.447-.1-.661L19.24 5.338a2.25 2.25 0 0 0-2.15-1.588H6.911a2.25 2.25 0 0 0-2.15 1.588L2.35 13.177a2.25 2.25 0 0 0-.1.661Z" /></svg> <a href="https://kwa.robotik.ag" target="_blank">kwa</a>
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="size-6" style="height: 1em; width: 1em"><path stroke-linecap="round" stroke-linejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 5.25h.008v.008H12v-.008Z" /></svg> <a href="https://ismyinternetworking.com" target="_blank">Is my internet working?</a>
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="size-6" style="height: 1em; width: 1em"><path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" /></svg> <a href="http://192.168.0.200:8123" target="_blank">HomeAssistant</a>
+`;
+
+
+let charIndex = 0;
+
+function typeCommand() {
+    if (charIndex < command.length) {
+        container.innerHTML += command.charAt(charIndex);
+        charIndex++;
+        const typingSpeed = 50 + Math.random() * 100; // random for realism
+        setTimeout(typeCommand, typingSpeed);
+    } else {
+        // Command finished, show result after short pause
+        setTimeout(() => {
+            container.innerHTML += "<br>" + result.replace(/\n/g, "<br>");
+        }, 300); // pause before showing result
+    }
+}
+
+typeCommand();
